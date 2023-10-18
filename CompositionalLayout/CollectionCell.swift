3595c1d3cebc8978632c8b64c6025fac2ffe6ccf
sykarda <ardailgili@outlook.com>
2023-10-18 14:00:30 +0300

    Basic CollectionView configuration added.

---
+++ CompositionalLayout/CollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionCell.swift
+//  CompositionalLayout
+//
+//  Created by Arda Ilgili on 18.10.2023.
+//
+
+import UIKit
+
+class CollectionCell: UICollectionViewCell {
+    static let identifier = "CollectionCell"
+    
+    func configure() {
+        contentView.backgroundColor = .red
+    }
+}
--- CompositionalLayout/ViewController.swift
+++ CompositionalLayout/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.register(CollectionCell.self, forCellWithReuseIdentifier: CollectionCell.identifier)
+
+        var layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 150, height: 150)
+        layout.scrollDirection = .horizontal
+
+        collectionView.collectionViewLayout = layout
     }
 
+}
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionCell.identifier, for: indexPath) as? CollectionCell else { return UICollectionViewCell() }
+
+        cell.configure()
+        return cell
+    }
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        1
+    }
 }
 
